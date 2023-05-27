a860d86ecb1db78b85677c2cd0748784d1d5adfb
filipp.kucherov <filipp.kucherov@r1.team>
2023-05-27 11:05:51 +0700

    3. Массивы и модели. [#174892] fix

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -19,7 +19,7 @@ public class Library {
         }
         System.out.println("А тут вывел только книги с нужным именем");
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean Code")) {
+            if ("Clean Code".equals(books[i].getName())) {
                 System.out.println(books[i].getName());
             }
         }
