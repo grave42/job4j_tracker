05b3eeff4ee1b6c0e1808830b1691bce18cc2919
filipp.kucherov <filipp.kucherov@r1.team>
2023-05-28 13:09:16 +0700

    7. Метод удаления заявки Tracker.delete [#211749] fix

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -62,9 +62,7 @@ public class Tracker {
         boolean rsl = false;
         int index = indexOf(id);
         if (index != -1) {
-            int start = index + 1;
-            int lenght = items.length - index - 1;
-            System.arraycopy(items, start, items, index, lenght);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             rsl = true;
