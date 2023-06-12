f133a9367c52ceb9a6732ec3a2a0799e703c6d98
filipp.kucherov <filipp.kucerov@r1.team>
2023-06-12 11:07:02 +0700

    Revert "0. Что такое исключение. [#3821  [#3821]] fix"
    This reverts commit a9023cbdc5451c28a028511d7fececda35db11e5.

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class BackArray {
     public static void main(String[] args) {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        int lol = names.length;
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
             String temp = names[index];
