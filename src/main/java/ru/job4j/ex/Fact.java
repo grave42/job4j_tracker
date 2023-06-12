a6598e02a44ad1719f5a5ba81af6298e95ce5e6f
filipp.kucherov <filipp.kucerov@r1.team>
2023-06-12 11:57:41 +0700

    0.3. Тестирование исключений с junit [#219366  [#219366]]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,7 +1,7 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public int calc(int n) {
+    public static int calc(int n) {
         int rsl = 1;
         if (n < 0) {
             throw new IllegalArgumentException("N could not be less than 0");
