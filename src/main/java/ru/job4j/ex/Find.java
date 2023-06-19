557c502545b880a52154e6d7fd956f7a090bc5dd
filipp.kucherov <filipp.kucerov@r1.team>
2023-06-19 23:08:25 +0700

    3. Замена if-else-throw на if-throw [#49895  [#49895 #423833]] fix 2

--- src/main/java/ru/job4j/ex/Find.java
+++ src/main/java/ru/job4j/ex/Find.java
@@ -2,15 +2,15 @@ package ru.job4j.ex;
 
 public class Find {
     public static String get(String[] data, int index) {
-        if (index >= 0 && index < data.length) {
-            return data[index];
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("Index out of bound");
         }
-        throw new IllegalArgumentException("Index out of bound");
+        return data[index];
     }
 
     public static void main(String[] args) {
         String[] data = {"one", "two", "three"};
-        String rsl = Find.get(data, 0);
+        String rsl = Find.get(data, 3);
         System.out.println(rsl);
     }
 }
