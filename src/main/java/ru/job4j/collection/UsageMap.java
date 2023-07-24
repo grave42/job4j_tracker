f5ea6e4b1b3e06643384a007b7481077f97e3205
feel <filipp.kucherov@r1.team>
2023-07-24 16:26:02 +0700

    0. Map, HashMap [#212652  [#212652 #431894]] fix

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,7 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("dod1k@po4ta.ru", "Dodikov Valeriy Dodikovich");
+        map.put("dod1k@po4ta.ru", "Viktorov Viktor Viktorovich");
         map.put("bob1k@ko4ka.ru", "Bobikov Vasiliy Sergeevich");
+        map.put("ya@lol.ru", "Ivanov Ivan Ivanovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " : " + value);
