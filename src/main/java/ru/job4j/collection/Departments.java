2e35f1f0b733a1cacb679ada07bf57a89104fa95
filipp.kucherov <filipp.kucerov@r1.team>
2023-08-16 16:53:24 +0700

    3. Отсортировать департаменты [#34352] fix 2

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,12 +9,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start.isEmpty()) {
-                    start = el;
-                } else {
-                    start += "/" + el;
-                }
-                tmp.add(start.isEmpty() ? el : start);
+                start += start.isEmpty() ? el : "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
