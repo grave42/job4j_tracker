5f486a6e4cb1dd8796159cad78309680db60965a
Филипп Кучеров <filipp.kucherov@gazprombank.ru>
2024-08-19 16:44:44 +0700

    1. Lombok. Настройка IDEA [#504888] fix

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 public class Item {
     private int id;
     private String name;
+    @EqualsAndHashCode.Exclude
     private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
