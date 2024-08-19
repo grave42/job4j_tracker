0aa231fc1a6f6e2c037515c5bba559e86542ca71
Филипп Кучеров <filipp.kucherov@gazprombank.ru>
2024-08-19 16:19:50 +0700

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,11 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+@Data
 public class Item {
     private int id;
     private String name;
@@ -20,51 +22,5 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
-                + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
